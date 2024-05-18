419fe4204af2f4e40077f52a29e6767aae9e4074
suvajit <suvajit.me@gmail.com>
2024-05-18 13:42:02 +0530

    Remove github actions matrix

--- backup.sh
+++ backup.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 set -e
 
-DATE=$(date +%Y-%m-%d-%H-%M-%S)
+DATE=$(date +%Y-%m-%dT%H-%M)
 BACKUP_DIR="backup"
 
 echo "Starting mongodump..."
 
-mongodump --host $MONGO_HOST \
-    --port $MONGO_PORT \
-    --username $MONGO_USERNAME \
-    --password $MONGO_PASSWORD \
-    --authenticationDatabase admin \
-    --db $MONGO_DB_NAME \
-    --collection $MONGO_COLLECTION_NAME \
-    --out ./$BACKUP_DIR
+COLLECTIONS=("PlayerLinks" "Users" "ClanStores" "LastSeen")
 
-tar -czf "$BACKUP_DIR-$DATE.tar.gz" -c "$BACKUP_DIR"
+for COLLECTION in "${COLLECTIONS[@]}";
+do
+    mongodump --host $MONGO_HOST \
+        --port $MONGO_PORT \
+        --username $MONGO_USERNAME \
+        --password $MONGO_PASSWORD \
+        --authenticationDatabase admin \
+        --db $MONGO_DB_NAME \
+        --collection $COLLECTION \
+        --out ./$BACKUP_DIR
+done
+
+tar -czf "$DATE.tar.gz" -c "$BACKUP_DIR"
 
 echo "Uploading backup to S3..."
-aws s3 cp "./$BACKUP_DIR-$DATE.tar.gz" "s3://$S3_BUCKET/$MONGO_COLLECTION_NAME-$DATE.tar.gz"
+aws s3 cp "./$DATE.tar.gz" "s3://$S3_BUCKET/$DATE.tar.gz"
 
 echo "Backup and upload completed successfully."
